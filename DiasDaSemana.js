49b3e2a9333a83c5785ee47ff02b6ef4f2a879e3
Abivisu2 <abiskabvisu@gmail.com>
2025-04-18 17:02:12 -0300

    Atualização da atividade sobre dias da semana

--- DiasDaSemana.js
+++ DiasDaSemana.js
@@ -1,1 +1,26 @@
-//2) Crie uma funcao para que escreva os dias da semana a partir do numeral (1 a 7) recebido por parametro.
+//2) Crie uma função para que escreva os dias da semana a partir do numeral (1 a 7), recebido por parametro.
+
+function DiasDaSemana(numero) {
+    
+    const Dias = [
+        "Domingo",
+        "Segunda-feira",
+        "Terça-feira",
+        "Quarta-feira",
+        "Quinta-feira",
+        "Sexta-feira",
+        "Sábado"
+    ]
+
+    if(numero > 1 & numero <= 7){
+        return Dias[numero - 1]
+    }else{
+        return "Número inválido. Digite um valor de 1 a 7.";
+    }
+}
+
+console.log(DiasDaSemana(2))
+console.log(DiasDaSemana(4))
+console.log(DiasDaSemana(0))
+console.log(DiasDaSemana(2))
+
